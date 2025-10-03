968f415db050fee346002c4a755f2c8a93b5a00e
santos-eng <techatarian@gmail.com>
2025-10-03 19:21:41 +0930

    initial rng lane functionality

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 
 class Player {
     int x, y;
@@ -12,30 +13,73 @@ public:
 };
 
 class Lane {
-    
+    // Obstacles opposing the player
+    std::deque<bool> cars;
+public:
+    Lane(int width) {
+        for (int i = 0; i < width; i++) {
+            // init
+            cars.push_front(false);
+        }
+    }
+
+    // 1 in 10 chance of car appearing from the left
+    void drive() {
+        if (rand() % 10 == 1)
+            cars.push_front(true);
+        else
+            cars.push_front(false);
+        
+        // maintain width
+        cars.pop_back();
+    }
+
+    bool checkPos(int pos) {
+        return cars[pos];
+    }
 };
 
 class Game {
     bool quit;
 public:
-    void Draw() {
+    void draw() {
 
     }
 
-    void Input() {
+    void input() {
 
     }
 
-    void Logic() {
+    void logic() {
 
     }
 
-    void Run() {
+    void run() {
         while (!quit) {
-            Input();
-            Draw();
-            Logic();
+            input();
+            draw();
+            logic();
         }
 
     }
 };
+
+int main() {
+    Lane lane1(20);
+
+    for (int i = 0; i < 20; i++) {
+        if (lane1.checkPos(i)) {
+            std::cout << "1";
+        } else {
+            std::cout << "0";
+        }
+    }
+    lane1.drive();
+    for (int i = 0; i < 20; i++) {
+        if (lane1.checkPos(i)) {
+            std::cout << "1";
+        } else {
+            std::cout << "0";
+        }
+    }
+}
